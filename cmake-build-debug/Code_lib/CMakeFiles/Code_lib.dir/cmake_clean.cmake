c511091b2aa02c1e474761e12ba405a049a5223b
Saif-Hashish <saifhashish364@gmail.com>
2024-11-03 23:50:22 +0200

    submission assignment

---
+++ cmake-build-debug/Code_lib/CMakeFiles/Code_lib.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Code_lib.dir/BST.cpp.obj"
+  "CMakeFiles/Code_lib.dir/BST.cpp.obj.d"
+  "CMakeFiles/Code_lib.dir/Dictionary.cpp.obj"
+  "CMakeFiles/Code_lib.dir/Dictionary.cpp.obj.d"
+  "libCode_lib.a"
+  "libCode_lib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Code_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/Google_tests/CMakeFiles/Google_Tests_run.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Google_Tests_run.dir/TestBST.cpp.obj"
+  "CMakeFiles/Google_Tests_run.dir/TestBST.cpp.obj.d"
+  "CMakeFiles/Google_Tests_run.dir/TestDictionary.cpp.obj"
+  "CMakeFiles/Google_Tests_run.dir/TestDictionary.cpp.obj.d"
+  "Google_Tests_run.exe"
+  "Google_Tests_run.exe.manifest"
+  "Google_Tests_run.pdb"
+  "libGoogle_Tests_run.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Google_Tests_run.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
